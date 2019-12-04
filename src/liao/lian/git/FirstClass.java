68ab95c8091bb96bc9d54096a827a0e1940486fc
lian <coderliao@qq.com>
2019-12-04 19:07:18 +0800

    把本地工程添加到git

---
+++ src/liao/lian/git/FirstClass.java
@@ -0,0 +1,16 @@
+package liao.lian.git;
+
+/**
+ * @author LiaoLian
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
